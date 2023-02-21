e999cb63291674c5f0b2344aa516c93fd65b9563
SCH-YcHan <ychan.sch@gmail.com>
2023-02-21 22:45:35 +0900

    read_data

--- Read_SAS_Data.R
+++ Read_SAS_Data.R
@@ -5,7 +5,56 @@ rm(list=ls())
 #call library
 library(sas7bdat)
 
-#read data
-test <- read.sas7bdat("")
+#set dir
+setwd("C:/Users/user/Desktop/KHP/data")
 
-list.files("../data")
+#read data
+mth <- read.sas7bdat("mt08_h.sas7bdat")
+head(mth)
+
+mti <- read.sas7bdat("mt08_i.sas7bdat")
+head(mti)
+
+hh <- read.sas7bdat("t08hh.sas7bdat")
+head(hh)
+
+ind <- read.sas7bdat("t08ind.sas7bdat")
+head(ind)
+
+hph <- read.sas7bdat("t08ahph.sas7bdat")
+head(hph)
+
+phi <- read.sas7bdat("t08aphi.sas7bdat")
+head(phi)
+
+phr <- read.sas7bdat("t08aphr.sas7bdat")
+head(phr)
+
+cd <- read.sas7bdat("t08cd.sas7bdat")
+head(cd)
+
+md <- read.sas7bdat("t08md.sas7bdat")
+head(md)
+
+er <- read.sas7bdat("t08er.sas7bdat")
+head(er)
+
+in_ <- read.sas7bdat("t08in.sas7bdat")
+head(in_)
+
+ou <- read.sas7bdat("t08ou.sas7bdat")
+head(ou)
+
+appen <- read.sas7bdat("t09appen.sas7bdat")
+head(appen)
+
+ltc <- read.sas7bdat("t09ltc.sas7bdat")
+head(ltc)
+
+income_ind <- read.sas7bdat("t09income_ind.sas7bdat")
+head(income_ind)
+
+
+
+
+
